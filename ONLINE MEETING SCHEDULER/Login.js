a4b731c917bc220dee4d458069d450ff8e040ba9
jayanivethan1420 <146815200+jayanivethan1420@users.noreply.github.com>
2024-11-13 18:17:09 +0530

    Create Login.js

---
+++ ONLINE MEETING SCHEDULER/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BasicForm from './BasicForm';
+
+function App() {
+  return (
+    <div>
+      <h1>User Feedback Form</h1>
+      <BasicForm />
+    </div>
+  );
+}
+
+export default App;
